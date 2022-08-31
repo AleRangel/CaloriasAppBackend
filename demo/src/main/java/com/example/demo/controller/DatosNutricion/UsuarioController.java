10c0c73cae5d0873f0ec3e0f54ed400ddcead30a
AleRangel <ale.andrade.rangel@gmail.com>
2022-08-31 11:00:52 -0500

    usuarioControlle

--- demo/src/main/java/com/example/demo/controller/DatosNutricion/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controller/DatosNutricion/UsuarioController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller.DatosNutricion;
 
 import java.util.List;
 
-import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,8 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
